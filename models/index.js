0e33bc07ec845defd61282a6bec5501f3552defd
Averi <averi.sherman@gmail.com>
2022-09-30 13:55:30 -0400

    model index.js

--- models/index.js
+++ models/index.js
@@ -2,35 +2,13 @@
 const User = require("./User");
 const Data = require("./Data");
 
+User.hasMany(Data, {
+    foreignKey: "user_id",
+    onDelete: "CASCADE"
+});
 
+Data.belongsTo(User, {
+    foreignKey: "user_id",
+});
 
-
-
-// Post.belongsTo(User, {
-//     foreignKey: "author_id",
-// });
-
-// User.hasMany(Post, {
-// foreignKey: "author_id",
-// onDelete: "CASCADE",
-// });
-
-// Comment.belongsTo(Post, {
-//     foreignKey: "post_id",
-// });
-
-// Post.hasMany(Comment, {
-//     foreignKey: "post_id",
-//     onDelete: "CASCADE",
-// });
-
-// Comment.belongsTo(User, {
-//     foreignKey: "author_id",
-// });
-
-// User.hasMany(Comment, {
-//     foreignKey: "author_id",
-//     onDelete: "CASCADE",
-// });
-
-// module.exports = {User, Post, Comment};
+module.exports = {User, Data};
