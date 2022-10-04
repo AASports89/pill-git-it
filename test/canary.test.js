277071fb2722ea97cfd9a6e5f429a449f4b023ff
AASports89 <aavakyan16@students.kgi.edu>
2022-10-04 11:51:15 -0700

    updated file pathways

--- test/canary.test.js
+++
@@ -1,8 +0,0 @@
-//DEPENDENCY//
-  var expect = require("chai").expect;
-//CANARY --> ALWAYS PASS TESTS --> SETS UP TESTING SUITE//
-  describe("canary test", function() {
-    it("should pass this canary test", function() {
-      expect(true).to.be.true;
-    });
-  });
