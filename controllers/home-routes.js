4427ceeac73db95d592b6558cf6a0450b5455377
seanfitz34 <seanwilliamfitzgerald@gmail.com>
2022-10-04 10:27:53 -0700

    updating sign up form

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,15 +1,15 @@
-const router = require('express').Router();
-const { User, PillData } = require('../models');
-const withAuth = require('../utils/auth');
+const router = require("express").Router();
+const { User, PillData } = require("../models");
+const withAuth = require("../utils/auth");
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     // Get all projects and JOIN with user data
     const dbpillData = await PillData.findAll({
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
@@ -18,31 +18,31 @@ router.get('/', async (req, res) => {
     const pills = dbpillData.map((pill) => pill.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('homepage', { 
-      pills, 
-      logged_in: req.session.logged_in 
+    res.render("homepage", {
+      pills,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/pillData/:id', async (req, res) => {
+router.get("/pillData/:id", async (req, res) => {
   try {
     const dbpillData = await PillData.findByPk(req.params.id, {
       include: [
         {
           model: User,
-          attributes: ['name'],
+          attributes: ["name"],
         },
       ],
     });
-  // Serialize data so the template can read it
+    // Serialize data so the template can read it
     const pills = dbpillData.get({ plain: true });
 
-    res.render('pill', {
+    res.render("pill", {
       ...pills,
-      logged_in: req.session.logged_in
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -50,33 +50,33 @@ router.get('/pillData/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/user', withAuth, async (req, res) => {
+router.get("/user", withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
-      attributes: { exclude: ['password'] },
+      attributes: { exclude: ["password"] },
       include: [{ model: Project }],
     });
 
     const user = userData.get({ plain: true });
 
-    res.render('profile', {
+    res.render("profile", {
       ...user,
-      logged_in: true
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
   }
 });
 
-router.get('/login', (req, res) => {
+router.get("/login", (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/profile');
+    res.redirect("/profile");
     return;
   }
 
-  res.render('login');
+  res.render("login");
 });
 
 module.exports = router;
--- public/js/login.js
+++ public/js/login.js
@@ -25,14 +25,13 @@ const loginFormHandler = async (event) => {
 const signupFormHandler = async (event) => {
     event.preventDefault();
 
-    const name = document.querySelector('#name-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const email = document.querySelector('#email-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
 
-    if (name && email && password) {
+    if (email && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
